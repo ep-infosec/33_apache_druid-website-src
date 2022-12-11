a84747174191f995cb2a0e226374ce299bec5704
ep-infosec <ep-infosec@users.noreply.github.com>
2022-12-11 21:51:27 +0200

    init

---
+++ README.md
@@ -0,0 +1,29 @@
+Druid Project Website
+=====================
+
+https://druid.apache.org/
+
+## Building
+
+Setup (you only need to do this once):
+
+```
+npm install
+bundle install
+```
+
+Every time you want to run the site:
+
+```
+gulp
+npm start
+```
+
+## Notes
+
+Ideally we would not be checking in the `css` directory and just build it as part of the deploy process.
+
+## Contributing
+
+See [CONTRIBUTING.md](https://github.com/apache/druid-website-src/blob/master/CONTRIBUTING.md).
+
---
+++ release.sh
@@ -0,0 +1,47 @@
+#!/bin/bash -eu
+
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+if [ "$#" -ne 2 ]; then
+  echo "Illegal number of parameters, should be two: ./release.sh version commitish";
+  exit 1;
+fi
+
+echo "Building to docs for '$1'..."
+
+baseDir=$(pwd)
+
+pushd $baseDir
+rm -rf _staging
+mkdir -p _staging
+cd _staging
+git clone -b "$2" --single-branch --depth 1 git@github.com:apache/druid.git
+cd druid
+git checkout "$2"
+mvn -Pwebsite-docs -pl website compile "-Dwebsite.src=$baseDir" "-Dwebsite.version=$1" -Dcheckstyle.skip=true
+
+popd
+
+git add .
+
+echo "Building Website..."
+npm run build
+pushd $(pwd)
+cd ../druid-website/
+git add .
+popd
